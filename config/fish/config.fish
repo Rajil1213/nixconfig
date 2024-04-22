a241679b9bb708f18a5cd6834475439465e4efdc
Rajil Bajracharya <imrb1213@gmail.com>
2024-04-22 16:35:32 +0545

    Fixup paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,28 +1,28 @@
-set PATH $HOME/Library/pnpm $PATH
-set PATH /usr/local/bin $PATH
-set PATH $HOME/.cargo/bin $PATH
-set PATH $HOME/nightly/nvim-macos/bin $PATH
-set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-
-set -x PNPM_HOME $HOME/Library/pnpm
-
-set -x GOPATH $HOME/go
-set PATH $GOPATH/bin $PATH
-
-set -x PYENV_ROOT $HOME/.pyenv
-set PATH $PYENV_ROOT/bin $PATH
+# set PATH $HOME/Library/pnpm $PATH
+# set PATH /usr/local/bin $PATH
+# set PATH $HOME/.cargo/bin $PATH
+# set PATH $HOME/nightly/nvim-macos/bin $PATH
+# set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+# 
+# set -x PNPM_HOME $HOME/Library/pnpm
+# 
+# set -x GOPATH $HOME/go
+# set PATH $GOPATH/bin $PATH
+# 
+# set -x PYENV_ROOT $HOME/.pyenv
+# set PATH $PYENV_ROOT/bin $PATH
 
 set -U EDITOR "vi"
 
 alias vi="nvim"
 alias vim="nvim"
 alias lg="lazygit"
-alias youtube-dl='python3 /usr/local/bin/youtube-dl'
+# alias youtube-dl='python3 /usr/local/bin/youtube-dl'
 alias netchk="ping 8.8.8.8"
 alias vpnchk="ping 10.99.0.1"
 alias git-graph="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 alias ls='exa --icons -F -H --group-directories-first --git -1'
 
-/usr/local/bin/starship init fish | source
-/usr/local/bin/kubectl completion fish | source
-/Users/rajil/.cargo/bin/zoxide init --cmd cd fish | source
+starship init fish | source
+# /usr/local/bin/kubectl completion fish | source
+zoxide init --cmd cd fish | source
