887995572f3a16d04b4880255a408294582d1e7c
Rajil Bajracharya <imrb1213@gmail.com>
2024-04-22 16:47:30 +0545

    fix: use full path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -23,6 +23,6 @@ alias netchk="ping 8.8.8.8"
 alias git-graph="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 alias ls='eza --icons -F -H --group-directories-first --git -1'
 
-$HOME/.nix-profile/bin/starship init fish | source
+/Users/rajil/.nix-profile/bin/starship init fish | source
 # /usr/local/bin/kubectl completion fish | source
-$HOME/.nix-profile/bin/zoxide init --cmd cd fish | source
+/Users/rajil/.nix-profile/bin/zoxide init --cmd cd fish | source
