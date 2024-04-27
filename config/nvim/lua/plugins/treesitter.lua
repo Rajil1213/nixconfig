621e8cd5de753d3840bff5fad203a8823fc23a8f
Rajil Bajracharya <imrb1213@gmail.com>
2024-04-27 15:35:15 +0545

    Add foundry and treesitter-solidity

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,18 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-      "lua", "ron", "rust", "toml", "go", "gomod", "gowork", "gosum", "ninja", "python", "rst"
+      "lua",
+      "ron",
+      "rust",
+      "toml",
+      "go",
+      "gomod",
+      "gowork",
+      "gosum",
+      "ninja",
+      "python",
+      "rst",
+      "solidity",
     })
   end,
 }
