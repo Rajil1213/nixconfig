974f48f6c3e6e71b45b4400a8fdaaac46ac16118
Rajil Bajracharya <imrb1213@gmail.com>
2024-04-29 02:08:17 +0545

    Add efm and solhint

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -16,6 +16,7 @@ return {
         "rust_analyzer",
         "tsserver",
         "vimls",
+        "efm",
       })
     end,
   },
@@ -36,6 +37,7 @@ return {
         "gotests",
         "isort",
         "stylua",
+        "solhint",
       })
     end,
   },
