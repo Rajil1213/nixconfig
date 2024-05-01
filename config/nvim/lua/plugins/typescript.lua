08c459be85e60b169941020084f0a94e067f8a84
Rajil Bajracharya <imrb1213@gmail.com>
2024-05-01 14:33:07 +0545

    Skip prettier

--- config/nvim/lua/plugins/typescript.lua
+++ config/nvim/lua/plugins/typescript.lua
@@ -35,36 +35,7 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "tsserver", "eslint_d" })
-    end,
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "prettierd" })
-      if not opts.handlers then opts.handlers = {} end
-
-      local has_prettier = function(util)
-        return check_json_key_exists(vim.fn.getcwd() .. "/package.json", "prettier")
-          or util.root_has_file ".prettierrc"
-          or util.root_has_file ".prettierrc.json"
-          or util.root_has_file ".prettierrc.yml"
-          or util.root_has_file ".prettierrc.yaml"
-          or util.root_has_file ".prettierrc.json5"
-          or util.root_has_file ".prettierrc.js"
-          or util.root_has_file ".prettierrc.cjs"
-          or util.root_has_file "prettier.config.js"
-          or util.root_has_file ".prettierrc.mjs"
-          or util.root_has_file "prettier.config.mjs"
-          or util.root_has_file "prettier.config.cjs"
-          or util.root_has_file ".prettierrc.toml"
-      end
-
-      opts.handlers.prettierd = function()
-        local null_ls = require "null-ls"
-        null_ls.register(null_ls.builtins.formatting.prettierd.with { condition = has_prettier })
-      end
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "tsserver", "eslint" })
     end,
   },
   {
