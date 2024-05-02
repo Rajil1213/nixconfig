5975d966e35d98469df41b91aad5970a001f63c3
Rajil Bajracharya <imrb1213@gmail.com>
2024-05-02 14:46:03 +0545

    Add buildInputs as a comment
    Uncomment when resolved

--- flake.nix
+++ flake.nix
@@ -16,6 +16,26 @@
   # In this context, outputs are mostly about getting home-manager what it
   # needs since it will be the one using the flake
   outputs = { nixpkgs, home-manager, ... }: {
+    # the following are not working for some reason
+    # TODO: Get these to work
+    # nativeBuildInputs = with nixpkgs.legacyPackages.aarch64-darwin; [
+    #   llvm
+    #   darwin.apple_sdk.frameworks.System
+    #   llvmPackages.libclang
+    #   pkg-config
+    # ];
+    # buildInputs = with nixpkgs.legacyPackages.aarch64-darwin; [
+    #   hidapi
+    #   llvm
+    #   llvmPackages.libclang
+    #   darwin.apple_sdk.frameworks.System
+    #   darwin.apple_sdk.frameworks.Security
+    #   darwin.apple_sdk.frameworks.CoreFoundation
+    #   darwin.apple_sdk.frameworks.CoreServices
+    #   openssl
+    #   zlib
+    # ];
+
     homeConfigurations = {
       "rajil" = home-manager.lib.homeManagerConfiguration {
         # darwin is the macOS kernel and aarch64 means ARM, i.e. apple silicon
