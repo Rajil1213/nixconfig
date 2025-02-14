1e7045bc43cd09d2efc9c971ab585ad35061f827
Rajil Bajracharya <imrb1213@gmail.com>
2025-02-14 15:14:27 +0545

    chore(term): remove broken ghostty pkg in mac

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
     # unstable has the 'freshest' packages you will find, even the AUR
     # doesn't do as good as this, and it's all precompiled.
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    ghostty.url = "github:ghostty-org/ghostty";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,27 +15,18 @@
 
   # In this context, outputs are mostly about getting home-manager what it
   # needs since it will be the one using the flake
-  outputs = { nixpkgs, home-manager, ghostty, ... }: {
+  outputs = { nixpkgs, home-manager, ... }: {
     # the following are not working for some reason
     homeConfigurations = {
       "rajil" = home-manager.lib.homeManagerConfiguration {
         # darwin is the macOS kernel and aarch64 means ARM, i.e. apple silicon
         pkgs = import nixpkgs {
           system = "aarch64-darwin";
-          overlays = [
-            (final: prev: {
-              ghostty = ghostty.packages.aarch64-darwin.default;
-            })  
-          ];
+          overlays = [];
         };
 
         modules = [ 
           ./home.nix 
-          ({ pkgs, ... }: {
-            home.packages = with pkgs; [
-              ghostty
-            ];
-          })
         ];
       };
     };
