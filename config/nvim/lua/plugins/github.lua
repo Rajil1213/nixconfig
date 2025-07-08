5d3990372a5ba62733cba94e65ac3b0ec7c55acf
Rajil Bajracharya <imrb1213@gmail.com>
2025-07-08 18:04:34 +0545

    feat(nvim): add octo

---
+++ config/nvim/lua/plugins/github.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-tree/nvim-web-devicons",
+    opts = {}
+  },
+
+  {
+    "pwntester/octo.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {}
+  }
+}
