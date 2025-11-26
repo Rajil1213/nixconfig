8b587815b57484c866c07dcdc5e4782288eed1f8
Rajil Bajracharya <imrb1213@gmail.com>
2025-11-26 15:28:57 +0545

    fix(git)!: use new git settings structure

--- home.nix
+++ home.nix
@@ -108,17 +108,16 @@
 
   programs.git = {
     enable = true;
-    userName = "Rajil Bajracharya";
-    userEmail = "imrb1213@gmail.com";
-    aliases = {
-      graph = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
-      s = "status";
-      ci = "commit";
-      co = "checkout";
-    };
-    extraConfig = {
-      pull = {
-        rebase = true;
+    settings = {
+      alias = {
+        graph = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
+        s = "status";
+        ci = "commit";
+        co = "checkout";
+      };
+      user = {
+        name = "Rajil Bajracharya";
+        email = "imrb1213@gmail.com";
       };
     };
   };
