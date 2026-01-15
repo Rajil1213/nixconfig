a158b1336dc9968ba5f7216238e24c70d43adfe9
Rajil Bajracharya <imrb1213@gmail.com>
2026-01-15 13:45:06 +0545

    feat(fish): migrate theme to config post 4.3.3

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -50,3 +50,33 @@ set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
 $HOME/.nix-profile/bin/starship init fish | source
 $HOME/.nix-profile/bin/zoxide init --cmd cd fish | source
 pyenv init - | source
+
+# THEME
+set fish_color_autosuggestion brblack
+set fish_color_cancel -r
+set fish_color_command blue
+set fish_color_comment red
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_end green
+set fish_color_error brred
+set fish_color_escape brcyan
+set fish_color_history_current --bold
+set fish_color_host normal
+set fish_color_host_remote yellow
+set fish_color_normal normal
+set fish_color_operator brcyan
+set fish_color_param cyan
+set fish_color_quote yellow
+set fish_color_redirection cyan --bold
+set fish_color_search_match white --background=brblack
+set fish_color_selection white --bold --background=brblack
+set fish_color_status red
+set fish_color_user brgreen
+set fish_color_valid_path --underline
+set fish_pager_color_completion normal
+set fish_pager_color_description yellow -i
+set fish_pager_color_prefix normal --bold --underline
+set fish_pager_color_progress brwhite --background=cyan
+set fish_pager_color_selected_background -r
+
